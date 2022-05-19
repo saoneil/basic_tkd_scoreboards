e448d5e3c07403634380b9d984004c54ba25a229
Sean O'Neil <40441698+saoneil@users.noreply.github.com>
2022-05-19 16:44:39 -0300

    sparring warnings/fouls

--- sparring_scoreboard_warn_foul_only.py
+++ sparring_scoreboard_warn_foul_only.py
@@ -34,9 +34,11 @@ root.grid_rowconfigure(8,weight=1)
 
 counter = 0
 running = False
-red_score = 0
-blue_score = 0
-tie_score = 0
+red_warnings = 0
+red_fouls = 0
+blue_warnings = 0
+blue_fouls = 0
+round_counter = 0
 
 def counter_label(label):
     def count():
@@ -70,61 +72,115 @@ def Reset(label):
         label['text'] = 'Ready'
     else:
         label['text'] = 'Starting...'
-def score_red_inc():
-    global red_score
-    red_score += 1
-    label_red_score.config(text=red_score)
-def score_red_dec():
-    global red_score
-    red_score -= 1
-    label_red_score.config(text=red_score)
-def score_blue_inc():
-    global blue_score
-    blue_score += 1
-    label_blue_score.config(text=blue_score)
-def score_blue_dec():
-    global blue_score
-    blue_score -= 1
-    label_blue_score.config(text=blue_score)
-def score_tie_inc():
-    global tie_score
-    tie_score += 1
-    label_tie_score.config(text=tie_score)
-def score_tie_dec():
-    global tie_score
-    tie_score -= 1
-    label_tie_score.config(text=tie_score)
+def warn_red_inc():
+    global red_warnings
+    red_warnings += 1
+    label_red_warnings.config(text=red_warnings)
+def warn_red_dec():
+    global red_warnings
+    red_warnings -= 1
+    label_red_warnings.config(text=red_warnings)
+def warn_blue_inc():
+    global blue_warnings
+    blue_warnings += 1
+    label_blue_warnings.config(text=blue_warnings)
+def warn_blue_dec():
+    global blue_warnings
+    blue_warnings -= 1
+    label_blue_warnings.config(text=blue_warnings)
+def round_counter_inc():
+    global round_counter
+    round_counter += 1
+    label_round_counter.config(text=round_counter)
+def round_counter_dec():
+    global round_counter
+    round_counter -= 1
+    label_round_counter.config(text=round_counter)
+def red_foul_inc():
+    global red_fouls
+    red_fouls += 1
+    label_red_fouls .config(text=red_fouls)
+def red_foul_dec():
+    global red_fouls
+    red_fouls -= 1
+    label_red_fouls .config(text=red_fouls)
+def blue_foul_inc():
+    global blue_fouls
+    blue_fouls += 1
+    label_blue_fouls .config(text=blue_fouls)
+def blue_foul_dec():
+    global blue_fouls
+    blue_fouls -= 1
+    label_blue_fouls .config(text=blue_fouls)
 def reset_all():
-    global red_score, blue_score, tie_score
-    red_score = 0
-    blue_score = 0
-    tie_score = 0
-    label_red_score.config(text=red_score)
-    label_blue_score.config(text=blue_score)
-    label_tie_score.config(text=tie_score)
+    global red_warnings, blue_warnings, round_counter, red_fouls, blue_fouls
+    red_warnings = 0
+    blue_warnings = 0
+    round_counter = 0
+    red_fouls = 0
+    blue_fouls = 0
+    label_red_warnings.config(text=red_warnings)
+    label_blue_warnings.config(text=blue_warnings)
+    label_round_counter.config(text=round_counter)
+    label_red_fouls.config(text=red_fouls)
+    label_blue_fouls.config(text=blue_fouls)
 
-time_label = tk.Label(root, text="Ready", bg='black', fg="white", font="Verdana 150 bold")
+time_label = tk.Label(root, text="Ready", bg='black', fg="white", font="Verdana 120 bold")
 start = tk.Button(root, text='Start', font="Verdana 10 bold", width=10, height=2, command=lambda: Start(time_label))
 stop = tk.Button(root, text='Stop', font="Verdana 10 bold", width=10, height=2, state='disabled', command=Stop)
 reset = tk.Button(root, text='Reset', font="Verdana 10 bold", width=10, height=2, state='disabled', command=lambda: Reset(time_label))
-label_red_score = tk.Label(root, text=red_score, font='Verdana 250 bold', bg='black', fg='white')
-label_blue_score = tk.Label(root, text=blue_score, font='Verdana 250 bold', bg='black', fg='white')
-label_tie_score = tk.Label(root, text=tie_score, font='Verdana 160 bold', bg='black', fg='white')
-redscoreinc = tk.Button(root, text="1 point", font='Verdana 15 bold', command=score_red_inc, width=6, height=1)
-redscoredec = tk.Button(root, text="1 point", font='Verdana 15 bold', command=score_red_dec, width=6, height=1)
-bluescoreinc = tk.Button(root, text="1 point", font='Verdana 15 bold', command=score_blue_inc, width=6, height=1)
-bluescoredec = tk.Button(root, text="1 point", font='Verdana 15 bold', command=score_blue_dec, width=6, height=1)
-reset_all = tk.Button(root, text='Reset Scores', command=reset_all, width=15)
 
+label_red_warnings = tk.Label(root, text=red_warnings, font='Verdana 220 bold', bg='black', fg='white')
+label_red_fouls = tk.Label(root, text=red_fouls, font='Verdana 150 bold', bg='black', fg='white')
+label_blue_warnings = tk.Label(root, text=blue_warnings, font='Verdana 220 bold', bg='black', fg='white')
+label_blue_fouls = tk.Label(root, text=blue_fouls, font='Verdana 150 bold', bg='black', fg='white')
+label_round_counter = tk.Label(root, text=round_counter, font='Verdana 100 bold', bg='black', fg='white')
+rw = tk.Label(root, text="Warnings", font='Verdana 10 bold', bg='black', fg='white')
+bw = tk.Label(root, text="Warnings", font='Verdana 10 bold', bg='black', fg='white')
+rf = tk.Label(root, text="Fouls", font='Verdana 10 bold', bg='black', fg='white')
+bf = tk.Label(root, text="Fouls", font='Verdana 10 bold', bg='black', fg='white')
+round = tk.Label(root, text="round", font='Verdana 10 bold', bg='black', fg='white')
+
+redwarninc = tk.Button(root, text="redwarning+", font='Verdana 5 bold', command=warn_red_inc, width=10, height=1)
+redwarndec = tk.Button(root, text="redwarning-", font='Verdana 5 bold', command=warn_red_dec, width=10, height=1)
+redfoulinc = tk.Button(root, text="redfoul+", font='Verdana 5 bold', command=red_foul_inc, width=10, height=1)
+redfouldec = tk.Button(root, text="redfoul-", font='Verdana 5 bold', command=red_foul_dec, width=10, height=1)
+bluewarninc = tk.Button(root, text="bluewarning+", font='Verdana 5 bold', command=warn_blue_inc, width=10, height=1)
+bluewarndec = tk.Button(root, text="bluewarning-", font='Verdana 5 bold', command=warn_blue_dec, width=10, height=1)
+bluefoulinc = tk.Button(root, text="bluefoul+", font='Verdana 5 bold', command=blue_foul_inc, width=10, height=1)
+bluefouldec = tk.Button(root, text="bluefoul-", font='Verdana 5 bold', command=blue_foul_dec, width=10, height=1)
+roundcounterinc = tk.Button(root, text="round+", font='Verdana 5 bold', command=round_counter_inc, width=9, height=1)
+roundcounterdec = tk.Button(root, text="round-", font='Verdana 5 bold', command=round_counter_dec, width=9, height=1)
+reset_all = tk.Button(root, text='Reset Scores', command=reset_all, width=15)
 
 time_label.grid(row=1, column=3)
 start.grid(row=6, column=3)
 stop.grid(row=7, column=3)
 reset.grid(row=8, column=3)
 reset_all.grid(row=9, column=3)
-label_red_score.grid(row=2, column=2)
-label_blue_score.grid(row=2, column=4)
-label_tie_score.grid(row=2, column=3)
+
+label_red_warnings.grid(row=2, column=2)
+label_red_fouls.grid(row=2, column=1)
+label_blue_warnings.grid(row=2, column=4)
+label_blue_fouls.grid(row=2, column=5)
+label_round_counter.grid(row=2, column=3)
+
+rw.place(relx=0.22, rely=0.20)
+rf.place(relx=0.07, rely=0.24)
+bw.place(relx=0.78, rely=0.20)
+bf.place(relx=0.93, rely=0.24)
+round.place(relx=0.50, rely=0.32)
+
+redwarninc.place(relx=0.22, rely=0.26)
+redwarndec.place(relx=0.22, rely=0.62)
+redfoulinc.place(relx=0.07, rely=0.30)
+redfouldec.place(relx=0.07, rely=0.55)
+bluewarninc.place(relx=0.78, rely=0.26)
+bluewarndec.place(relx=0.78, rely=0.62)
+bluefoulinc.place(relx=0.93, rely=0.30)
+bluefouldec.place(relx=0.93, rely=0.55)
+roundcounterinc.place(relx=0.5, rely=0.4)
+roundcounterdec.place(relx=0.5, rely=0.55)
 
 
 COMBINATIONS = []
@@ -143,27 +199,27 @@ def on_press(key):
     if any([key in COMBO for COMBO in red_inc]):
         current.add(key)
         if any(all(k in current for k in COMBO) for COMBO in red_inc):
-            score_red_inc()
+            warn_red_inc()
     elif any([key in COMBO for COMBO in blue_inc]):
         current.add(key)
         if any(all(k in current for k in COMBO) for COMBO in blue_inc):
-            score_blue_inc()
+            warn_blue_inc()
     elif any([key in COMBO for COMBO in red_dec]):
         current.add(key)
         if any(all(k in current for k in COMBO) for COMBO in red_dec):
-            score_red_dec()
+            warn_red_dec()
     elif any([key in COMBO for COMBO in blue_dec]):
         current.add(key)
         if any(all(k in current for k in COMBO) for COMBO in blue_dec):
-            score_blue_dec()
+            warn_blue_dec()
     elif any([key in COMBO for COMBO in tie_inc]):
         current.add(key)
         if any(all(k in current for k in COMBO) for COMBO in tie_inc):
-            score_tie_inc()
+            round_counter_inc()
     elif any([key in COMBO for COMBO in tie_dec]):
         current.add(key)
         if any(all(k in current for k in COMBO) for COMBO in tie_dec):
-            score_tie_dec()
+            round_counter_dec()
     elif any([key in COMBO for COMBO in timestart]):
         current.add(key)
         if any(all(k in current for k in COMBO) for COMBO in timestart):
